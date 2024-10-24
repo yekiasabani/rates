5323f85af5970bdcd91e75d75dbda52f58c4ed85
Esmaeil Keshtkar <Esmaeil@Esmaeils-MacBook-Pro.local>
2024-10-24 15:59:55 +0400

    2

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,13 +1,9 @@
+const {join} = require('path');
+
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Download Chrome (default `skipDownload: false`).
-  chrome: {
-    skipDownload: false,
-  },
-  // Download Firefox (default `skipDownload: true`).
-  firefox: {
-    skipDownload: false,
-  },
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
